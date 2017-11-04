6d6a094d6ec16c3dfde8a25d8cac8ba2de8cc7f9
NeddiePeng <576970513@qq.com>
2017-11-04 17:07:19 +0800

    增加商品分类业务

--- application/Home/Controller/Productsort.php
+++ application/Home/Controller/Productsort.php
@@ -8,6 +8,8 @@
 namespace app\home\controller;
 use app\home\model\Productsortm;
 use think\Controller;
+use think\Db;
+use think\Request;
 
 class Productsort extends Controller
 {
@@ -17,19 +19,100 @@ class Productsort extends Controller
      * 商品分类列表
      */
     public function Index(){
-          $power_str = "prosortlist";
-          if(!login_over_time()){
-              $this->redirect('home/login/index');
-          }
-          if(!_mate_power($power_str)){
-              $this->success('您没有权限进行该操作！','home/index/index');
-          }
-          $productsort_model = new Productsortm();
-          $get_sort_list = $productsort_model->get_sort_list();
+        $power_str = "prosortlist";
+        if(!login_over_time()){
+            $this->redirect('home/login/index');
+        }
+        if(!_mate_power($power_str)){
+            $this->success('您没有权限进行该操作！','home/index/index');
+        }
+        $productsort_model = new Productsortm();
+        $get_sort_list = $productsort_model->get_sort_list();
+        $this->assign('sort_data',$get_sort_list);
+        return $this->fetch('index');
+
+    }
+
+
+    /**
+     * 修改分类状态
+     */
+    public function Update_sort_status(){
+        $power_str = 'updatesortstatus';
+        if(!login_over_time()){
+            return json(['code' => -2,'url' => 'home/login/index']);
+        }
+        if(!_mate_power($power_str)){
+            $this->success('您没有权限进行该操作！','home/index/index');
+        }
+        $status = input('status');
+        $sort_id = input('sort_id');
+        $updata_data = [
+            'status' => $status
+        ];
+        $res = Db::table('e_product_sort')
+               ->where(['sort_id' => $sort_id])
+               ->update($updata_data);
+        return $res ? json(['code' => 1,'msg' => '修改成功！']) : json(['code' => -1,'msg' => '修改失败！']);
 
 
     }
 
+
+
+    /**
+     * 商品分类详情
+     */
+    public function Details_pro_sort(){
+        $power_str = "updateprosort";
+        if(!login_over_time()){
+            $this->redirect('home/login/index');
+        }
+        if(!_mate_power($power_str)){
+            $this->success('您没有权限进行该操作！','home/index/index');
+        }
+        $sort_id = input('sort_id');
+        $poSort_model = new Productsortm();
+        if(Request::instance()->post()){
+            $param = Request::instance()->post();
+            $sort_id = $param['sort_id'];
+            $sort_name = $param['sort_name'];
+            $update_sotr = $poSort_model->update_sort($sort_id,$sort_name);
+            return $update_sotr ? json(['code' => 1,'msg' => '修改成功！']) : json(['code' => -1,'msg' => '修改失败！']);
+        }
+        $get_pro_sort_info = $poSort_model->get_sort_info($sort_id);
+        $this->assign('one_data',$get_pro_sort_info);
+        return $this->fetch('details_sort');
+
+
+    }
+
+
+
+    /**
+     * 添加分类
+     */
+    public function Add_sort(){
+        $power_str = "addnewsort";
+        if(!login_over_time()){
+            return json(['code' => -2,'url' => 'home/login/index']);
+        }
+        if(!_mate_power($power_str)){
+            $this->success('您没有权限进行该操作！','home/index/index');
+        }
+        $sort_model = new Productsortm();
+        if(Request::instance()->post()){
+            $param = Request::instance()->post();
+            $old_data = Db::table('e_product_sort')->where(['sort_name' => $param['sort_name']])->select();
+            if(!empty($old_data)){
+                return json(['code' => -3,'msg' => '该分类已存在！']);
+            }
+            $add_sort = $sort_model->add_new_sort($param);
+            return $add_sort ? json(['code' => 1,'msg' => '添加成功！']) : json(['code' => -1,'msg' => '添加失败！']);
+        }
+        return $this->fetch('add_sort');
+
+    }
 
 
 
--- application/Home/model/Productsortm.php
+++ application/Home/model/Productsortm.php
@@ -22,9 +22,70 @@ class Productsortm extends Model
 
     /**
      * 获取分类
+     * @return array or null;
      */
     public function get_sort_list(){
         $data = Db::table('e_product_sort')->select();
+        return $data ? $data : null;
+    }
+
+
+    /**
+     * 获取分类信息
+     * @param  $sort_id  分类id
+     * @return array or  null;
+     */
+    public function get_sort_info($sort_id){
+        $one_data = Db::table('e_product_sort')
+                    ->where(['sort_id' => $sort_id])
+                    ->select();
+        return $one_data ? $one_data : null;
+    }
+
+
+    /**
+     * 修改分类信息
+     * @param $sort_id  分类id   分类名称
+     * @return true or false;
+     */
+    public function update_sort($sort_id,$sort_name){
+        $upate_data = [
+            'sort_name' => $sort_name
+        ];
+        $res = Db::table('e_product_sort')->where(['sort_id' => $sort_id])
+               ->update($upate_data);
+        return $res ? true : false;
+    }
+
+
+
+    /**
+     * 添加新的分类
+     * @param   $data  新分类数据
+     * @return true or false;
+     */
+    public function add_new_sort($data){
+        if(isset($data['status']) && $data['status'] == 'on'){
+            $status = 1;
+        }else{
+            $status = 3;
+        }
+        if($data['level'] == 1){
+            $insert_data = [
+                'sort_name' => $data['sort_name'],
+                'status' => $status
+            ];
+        }elseif($data['level'] == 2){
+            $insert_data = [
+                'parent_id' => $data['parent_id'],
+                'sort_name' => $data['sort_name'],
+                'status' => $status
+            ];
+        }
+
+        $res = Db::table('e_product_sort')->insert($insert_data);
+        return $res ? true : false;
+
     }
 
 
