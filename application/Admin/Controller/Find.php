cc737e41af73e63f9eb9364a06a3d0059c7da462
NeddiePeng <576970513@qq.com>
2017-11-09 14:31:00 +0800

    添加分类和作品API

---
+++ application/Admin/Controller/Find.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * 首页controller.
+ * User: Pengfan
+ * Date: 2017/11/9
+ * Time: 11:32
+ */
+namespace app\Admin\controller;
+use app\Admin\model\Navsort;
+use think\Controller;
+
+class  Find extends Controller
+{
+
+    /**
+     * 导航nav
+     */
+    public function Get_nav(){
+        $navsort_model = new Navsort();
+        $get_nav_list = $navsort_model->get_nav_list();
+        return $get_nav_list ? json(['code' => 1,'msg' => 'success','data' => $get_nav_list]) :
+                               json(['code' => -1,'msg' => 'fail']);
+    }
+
+
+    /**
+     * 获取nav_banner
+     */
+    public function Get_nav_banner(){
+        $nav_model = new Navsort();
+        $nav_id = input('nav_id');
+        if(empty($nav_id)){
+            return json(['code' => -2,'msg' => '导航id为空！']);
+        }
+        $get_banner = $nav_model->get_nav_banner($nav_id);
+        return $get_banner ? json(['code' => 1,'msg' => 'success','data' => $get_banner]) :
+                             json(['code' => -1,'msg' => 'fail']);
+
+    }
+
+
+    /**
+     * 获取分类导航内容
+     */
+    public function Get_nav_article(){
+        $nav_id = input('nav_id');
+        $page = input('page');
+        if(empty($nav_id) && empty($page)){
+            return json(['code' => -2,'msg' => '导航id和页数为空！']);
+        }
+        $navsort_model = new Navsort();
+        $get_article_list = $navsort_model->get_article_list($nav_id,$page);
+        return $get_article_list ? json(['code' => 1,'msg' => 'success','data' => $get_article_list]) :
+                                   json(['code' => -1,'msg' => 'fail']);
+
+    }
+
+
+
+
+}
---
+++ application/Admin/model/Navsort.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * 分类导航model.
+ * User: Pengfan
+ * Date: 2017/11/9
+ * Time: 11:38
+ */
+namespace app\Admin\model;
+use think\Db;
+use think\Model;
+
+class Navsort extends Model
+{
+
+    /**
+     * table
+     */
+    protected $table = 'e_nav_list';
+
+
+    /**
+     * 获取导航分类
+     * @return array or null;
+     */
+    public function get_nav_list(){
+        $data = Db::table('e_nav_list')
+                ->where(['status' => 1])
+                ->select();
+        return $data ? $data : null;
+    }
+
+
+    /**
+     * 获取导航banner
+     * @param $nav_id  导航id
+     * @return array or null;
+     */
+    public function get_nav_banner($nav_id){
+        $data = Db::table('e_nav_banner')
+                ->where(['nav_id' => $nav_id,'status' => 1])
+                ->select();
+        return $data ? $data : null;
+    }
+
+
+    /**
+     * 获取作品
+     * @param $nav_id  导航id
+     * @param $page    第几页
+     * @return array or null;
+     */
+    public function get_article_list($nav_id,$page){
+        $data = Db::table('e_article as ar')
+                ->join('e_article_info as ai','ai.article_id = ar.article_id')
+                ->join('e_user as us','ar.uid = us.uid')
+                ->where(['ar.nav_id' => $nav_id,'ar.status' => 1])
+                ->order('ar.add_time DESC')
+                ->field('ar.*,ai.content,ai.img_url,ai.music_url,ai.video_url,us.user_name')
+                ->limit($page,'10')
+                ->select();
+        return $data ? $data : null;
+
+    }
+
+
+}
